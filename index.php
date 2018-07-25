7b69f164648e0769aa4139226846c22fc2175d13
dembasy <dembasy1227@gmail.com>
2018-07-25 14:26:34 +0200

    done

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $lastName = "Demba";
+    $firstName = "Sylla";
+    $age = 23;
+    echo $lastName, $firstName, $age;
+      ?>
+  </body>
+</html>
